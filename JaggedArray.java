ba8081a8828f0061d2bed27a9b5bae6902a271f9
Vijendra Kumar Lodhi <vijendrarajpoot786n@gmail.com>
2026-01-28 23:07:46 +0530

    jagged array

---
+++ JaggedArray.java
@@ -0,0 +1,23 @@
+public class JaggedArray {
+    public static void main(String args[]){
+        int[][] array = new int[3][];
+
+        array[0] = new int[5];
+        array[1] = new int[4];
+        array[2] = new int[5];
+
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                array[i][j] = (int)(Math.random()*100);
+            }
+        }
+
+                for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                System.out.print(array[i][j]+ "  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
