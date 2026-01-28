0c5074b5da5914baa0eaf1c00c1edd264b2d26fc
Vijendra Kumar Lodhi <vijendrarajpoot786n@gmail.com>
2026-01-28 22:02:22 +0530

    piramid with two loops

---
+++ pattern{.java
@@ -0,0 +1,12 @@
+class pattern{
+    for(int i=0; i<7 ; i++){
+        for(int j=0; j<7; j++){
+            if(i==j){
+                System.out.println("*");
+            }
+            else{
+                System.out.println(" ");
+
+        }
+        }
+    }
---
+++ piramid.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class piramid {
+    
+    Scanner input = new Scanner(System.in);
+    public static void main(String args[])
+    {
+        System.out.println("Enter the no of cols : ");
+        Scanner input = new Scanner(System.in);
+
+        int data = input.nextInt();
+        int col = data-1;
+        int row  = col/2;
+       
+
+        for(int i=0; i<=row; i++){
+            for(int j=0; j<=col; j++){
+                if( row-i <= j && row+i >= j ){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+    
+
+}
