42d2ff6a854421042e5440d0cfb8b1bae01d5bdc
Vijendra Kumar Lodhi <vijendrarajpoot786n@gmail.com>
2026-01-28 22:47:01 +0530

    2d array

---
+++ TwoDarray.java
@@ -0,0 +1,20 @@
+public class TwoDarray {
+    public static void main(String args[]){
+        int row = 3;
+        int col = 5;
+        int[][] array = new int[row][col];
+
+        for(int i=0; i<array.length; i++){     
+            for(int j=0; j<array[i].length; j++){
+                array[i][j] = (int)(Math.random()*100);
+            }
+        }
+
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }    
+}
